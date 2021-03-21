2a2b123297d37cf6632dc9186788596f635d1eb6
TatsuyaNakamori <***>
2021-03-22 02:22:37 +0900

    Adding events (such as onDidChangeActiveTextEditor)

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,33 @@
+// ============================================================
+// Copyright (c) 2021 Tatsuya Nakamori. All rights reserved.
+// See LICENSE in the project root for license information.
+// ============================================================
 import * as vscode from 'vscode';
+import * as regexHighlighter from './regexHighlighter';
+
 
 export function activate(context: vscode.ExtensionContext) {
+    console.log('Congratulations, your extension "regexhighlighter" is now active!');
 
-	console.log('Congratulations, your extension "regexhighlighter" is now active!');
+    vscode.window.onDidChangeActiveTextEditor((editor) => {
+        if (editor) {
+            regexHighlighter.updateDecorations(editor);
+        }
+    }, null, context.subscriptions);
 
-	let disposable = vscode.commands.registerCommand('regexhighlighter.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from RegexHighlighter!');
-	});
+    vscode.workspace.onDidChangeTextDocument((event) => {
+        const editor = vscode.window.activeTextEditor;
+        if (editor && event.document === editor.document) {
+            regexHighlighter.updateDecorations(editor);
+        }
+    }, null, context.subscriptions);
 
-	context.subscriptions.push(disposable);
+    vscode.window.onDidChangeTextEditorSelection((event) => {
+        const editor = vscode.window.activeTextEditor;
+        if (editor && editor === event.textEditor) {
+            regexHighlighter.updateDecorations(editor);
+        }
+    }, null, context.subscriptions);
 }
 
 // this method is called when your extension is deactivated
