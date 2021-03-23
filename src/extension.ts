bff8f6aff897830022772ffefcc8346272e24190
TatsuyaNakamori <***>
2021-03-24 03:14:09 +0900

    Add Commands, Event, Init

--- src/extension.ts
+++ src/extension.ts
@@ -4,11 +4,30 @@
 // ============================================================
 import * as vscode from 'vscode';
 import * as regexHighlighter from './regexHighlighter';
+import * as config from './config';
 
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log('Congratulations, your extension "regexhighlighter" is now active!');
+    // console.log('Congratulations, your extension "regexhighlighter" is now active!');
 
+    // Run it once the first time.
+    config.setContext();
+
+    // ==========================
+    // Register Commands
+    // ==========================
+    context.subscriptions.push(
+        vscode.commands.registerCommand('regexhighlighter.stopHighlighting', () => {
+        config.setPauseConfig(true);
+    }));
+    context.subscriptions.push(
+        vscode.commands.registerCommand('regexhighlighter.resumeHighlighting', () => {
+        config.setPauseConfig(false);
+    }));
+
+    // ==========================
+    // Register Events
+    // ==========================
     vscode.window.onDidChangeActiveTextEditor((editor) => {
         if (editor) {
             regexHighlighter.updateDecorations(editor);
@@ -28,6 +47,22 @@ export function activate(context: vscode.ExtensionContext) {
             regexHighlighter.updateDecorations(editor);
         }
     }, null, context.subscriptions);
+
+    vscode.workspace.onDidChangeConfiguration((event) => {
+        config.setContext();
+
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            regexHighlighter.updateDecorations(editor, true);
+        }
+    }, null, context.subscriptions);
+
+    // ==========================
+    // Initialization
+    // ==========================
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) { return }
+    regexHighlighter.updateDecorations(editor);
 }
 
 // this method is called when your extension is deactivated
